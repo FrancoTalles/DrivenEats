fc2da27eb0b0539d0d47f55ab4ce34c11be31f09
FrancoTalles <francotallesextreme@gmail.com>
2022-08-15 15:51:10 -0400

    Projeto 3 v6

--- scripteats.js
+++ scripteats.js
@@ -6,6 +6,10 @@ console.log(teste1);
 function SelecionarComida(comida) {
   let tentativa = `${teste1}`;
   let final = "." + tentativa;
+  let nomedaclasse;
+  let numero;
+  nomedaclasse = comida.className;
+  numero = nomedaclasse[5];
   console.log(tentativa);
   console.log(final);
   const teste = document.querySelector(final);
@@ -15,10 +19,6 @@ function SelecionarComida(comida) {
   }
   console.log(teste);
   console.log(comida);
-  let nomedaclasse;
-  let numero;
-  nomedaclasse = comida.className;
-  numero = nomedaclasse[5];
   console.log(nomedaclasse);
   console.log(numero);
   let comidanomeclasse = `.nomeprato${numero}`;
@@ -113,16 +113,19 @@ function mudança() {
     const mudar = document.querySelector(".textofinal");
     const finalmudar = document.querySelector(".FinalDiv");
     mudar.innerHTML = "Fechar pedido";
-    mudar.classList.add("selecionado");
-    finalmudar.classList.add("selecionado");
+    mudar.classList.add("selecionadobotao2");
+    finalmudar.classList.add("selecionadobotao1");
   }
 }
 
 function enviarMensagem() {
   if (ativar === 3) {
+    somar();
     let celular = 55092994702103;
+    let nome = prompt("Qual é o seu nome?");
+    let endereço = prompt("Qual é o seu endereço?");
 
-    let texto = `Olá, gostaria de fazer o pedido:\n - Prato: ${comidaescolhida}\n - Bebida: ${bebidaescolhida}\n - Sobremesa: ${sobremesaescolhida}\n Total: R$ 27.70`;
+    let texto = `Olá, gostaria de fazer o pedido:\n - Prato: ${comidaescolhida}\n - Bebida: ${bebidaescolhida}\n - Sobremesa: ${sobremesaescolhida}\n Total: R$ ${total}\n \n Nome: ${nome}\n Endereço: ${endereço}`;
     texto = window.encodeURIComponent(texto);
 
     window.open(
@@ -130,4 +133,36 @@ function enviarMensagem() {
       "_blank"
     );
   }
+}
+
+let total = 0;
+
+function somar() {
+  let x = comidavalor;
+  x = x.replace("R$ ", "");
+  x = x.split(".").join("");
+  x = x.replace(",", ".");
+  x = Number(x);
+  console.log(typeof x);
+  console.log(x);
+
+  let y = bebidavalor;
+  y = y.replace("R$ ", "");
+  y = y.split(".").join("");
+  y = y.replace(",", ".");
+  y = Number(y);
+  console.log(typeof y);
+  console.log(y);
+
+  let z = sobremesavalor;
+  z = z.replace("R$ ", "");
+  z = z.split(".").join("");
+  z = z.replace(",", ".");
+  z = Number(z);
+  console.log(typeof z);
+  console.log(z);
+
+  total = x + y + z;
+  total = total.toFixed(2);
+  console.log(total);
 }
